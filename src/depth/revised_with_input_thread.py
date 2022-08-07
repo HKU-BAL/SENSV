5e70cb42ba58c302f13b69b0532ceec096d60e14
yifan <yifan.1021@gmail.com>
2022-08-07 19:33:56 +0800

    Revert "bug fix"
    This reverts commit 6ab0db21c8c8ed66f48fb61e4e5b4ccb8d84e609.

--- src/depth/revised_with_input_thread.py
+++ src/depth/revised_with_input_thread.py
@@ -76,8 +76,8 @@ def local_max_subarray(df, size, min_len=500000):
 def getDifByChr(chr_num, ref):
     """create a dataFrame for dif of log likelihood between two models"""
 
-    diflikhood_half = []
-    diflikhood_dup = []
+    diflikhood_half = pd.DataFrame(columns=['start', 'diflognorm'])
+    diflikhood_dup = pd.DataFrame(columns=['start', 'diflognorm'])
 
     for row in range(len(ref)):
         if ref[row][7] == chr_num and chr_num != 23 and chr_num != 24:
@@ -118,8 +118,8 @@ def getDifByChr(chr_num, ref):
             dif_dup = dupmeanlog-meanlog
             df_half = pd.DataFrame([[pos, dif_half]], columns=['start', 'diflognorm'])
             df_dup = pd.DataFrame([[pos, dif_dup]], columns=['start', 'diflognorm'])
-            diflikhood_half.append(df_half)
-            diflikhood_dup.append(df_dup)
+            diflikhood_half = diflikhood_half.append(df_half, ignore_index=True)
+            diflikhood_dup = diflikhood_dup.append(df_dup, ignore_index=True)
 
         elif ref[row][7] == chr_num and (chr_num == 23 or chr_num == 24):
             if args.gender == 'f':
@@ -163,17 +163,15 @@ def getDifByChr(chr_num, ref):
             dif_dup = dupmeanlog - meanlog
             df_half = pd.DataFrame([[pos, dif_half]], columns=['start', 'diflognorm'])
             df_dup = pd.DataFrame([[pos, dif_dup]], columns=['start', 'diflognorm'])
-            diflikhood_half.append(df_half)
-            diflikhood_dup.append(df_dup)
+            diflikhood_half = diflikhood_half.append(df_half, ignore_index=True)
+            diflikhood_dup = diflikhood_dup.append(df_dup, ignore_index=True)
 
-    diflikhood_half = pd.concat(diflikhood_half)
-    diflikhood_dup = pd.concat(diflikhood_dup)
     return diflikhood_half, diflikhood_dup
 
 
 def process(chrm, ref):
-    results_del = []
-    results_dup = []
+    results_del = pd.DataFrame(columns=['chr', 'start', 'end', 'sum of diflihood', 'del'])
+    results_dup = pd.DataFrame(columns=['chr', 'start', 'end', 'sum of diflihood', 'dup'])
 
     difByChr_half, difByChr_dup = getDifByChr(chrm, ref)
     regions_half = local_max_subarray(difByChr_half, len(difByChr_half.index), 500000)
@@ -188,7 +186,7 @@ def process(chrm, ref):
                 [[chrm, regions_half[i][1], regions_half[i][2], likelihood, "DEL"]],
                 columns=['chr', 'start', 'end', 'sum of diflihood', 'del']
             )
-            results_del.append(df)
+            results_del = results_del.append(df, ignore_index=True)
 
     for i in range(0, len(regions_dup)):
         likelihood = regions_dup[i][0]
@@ -198,18 +196,15 @@ def process(chrm, ref):
                 [[chrm, regions_dup[i][1], regions_dup[i][2], likelihood, "DUP"]],
                 columns=['chr', 'start', 'end', 'sum of diflihood', 'dup']
             )
-            results_dup.append(df)
-
-    results_del = pd.concat(results_del)
-    results_dup = pd.concat(results_dup)
+            results_dup = results_dup.append(df, ignore_index=True)
 
     return {'results_del': results_del, 'results_dup': results_dup}
 
 
 def main(args, ref):
     # drive code
-    results_del = []
-    results_dup = []
+    results_del = pd.DataFrame(columns=['chr', 'start', 'end', 'sum of diflihood', 'del'])
+    results_dup = pd.DataFrame(columns=['chr', 'start', 'end', 'sum of diflihood', 'dup'])
     end = -1
     if args.gender == 'f':
         end = 24
@@ -224,11 +219,8 @@ def main(args, ref):
     pool.join()
 
     for result in results:
-        results_del.append(result['results_del'])
-        results_dup.append(result['results_dup'])
-
-    results_del = pd.concat(results_del)
-    results_dup = pd.concat(results_dup)
+        results_del = results_del.append(result['results_del'], ignore_index=True)
+        results_dup = results_dup.append(result['results_dup'], ignore_index=True)
 
     output_prefix = ""
     if args.output_dir == "./" or args.output_dir == "/" or args.output_dir == ".":
